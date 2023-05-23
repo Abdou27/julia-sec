ba0eac5b1eff7b3846569d2b579be2d9a80d52a9
Rahim <benmelouka@live.fr>
2023-05-23 11:42:12 +0200

    Refactor

---
+++ functions.jl
@@ -0,0 +1,40 @@
+using Cbc, JuMP
+
+function P(g::Matrix{Int}, source::Int, target::Int)
+    # Size of the matrix
+    n = size(g, 1)
+
+    # Create an optimization model
+    model = Model(Cbc.Optimizer)
+
+    # The decision variables x[i, j], representing the flow on the arc (i, j) in the graph. 
+    # The variables are non-negative and bounded by the capacity g[i, j] of the corresponding arc.
+    @variable(model, 0 <= x[i = 1:n, j = 1:n] <= g[i, j])
+
+    # For each vertex i (excluding the source and target), this constraint ensures that the sum of flow entering i is equal to the sum of flow leaving i. 
+    for i in 1:n
+        if i ≠ source && i ≠ target
+            @constraint(model, sum(x[j, i] for j in 1:n if j ≠ i) == sum(x[i, j] for j in 1:n if j ≠ i))
+        end
+    end
+    # This constraint ensures that the sum of flow entering the source vertex is zero.
+    @constraint(model, sum(x[j, source] for j in 1:n) == 0)
+    # This constraint ensures that the sum of flow leaving the target vertex is zero.
+    @constraint(model, sum(x[target, j] for j in 1:n) == 0)
+
+    # The objective is to maximize the total flow leaving the source vertex (source).
+    @objective(model, Max, sum(x[source, j] for j in 1:n))
+
+    # Solve the optimization problem
+    MOI.set(model, MOI.Silent(), true)
+    optimize!(model)
+
+    # Check solution status
+    @assert termination_status(model) == MOI.OPTIMAL "No optimal solution found for the maximum flow problem"
+
+    # Retrieve maximum flow value and flow distribution
+    flow_value = round(Int, objective_value(model))
+    flow_distribution = round.(Int, value.(x))
+
+    return flow_value, flow_distribution
+end
---
+++ main.jl
@@ -0,0 +1,4 @@
+include("functions.jl")
+include("matrices.jl")
+
+flow_value, flow_distribution = P(G1, 1, 26)
